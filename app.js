2f0442adb702d69b94c5ee33585290b7528df5fd
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-28 15:13:58 -0400

    add a get dom string object to store all the tags that will be used in the functions

--- app.js
+++ app.js
@@ -7,21 +7,37 @@ var budgetController = (function(){
 
 
 // UI CONTROLLER
+// will select tags many times, to avoid typos, make all the tags a DOMstring
 var UIController = (function(){
+
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue:'.add__value',
+		inputBtn: '.add__btn'
+
+	}
 
 	return {
 		getInput: function(){
 // return an objects with the three input value, so that it can be used by other functions
 			return {
-				type : document.querySelector('.add__type').value,
-				description : document.querySelector('.add__description').value,
-				value : document.querySelector('.add__value').value
-			}
-			
+				type : document.querySelector(DOMstrings.inputType).value,
+				description : document.querySelector(DOMstrings.inputDescription).value,
+				value : document.querySelector(DOMstrings.inputValue).value
+			};
+		},
+// expose it
+		getDOMstrings: function(){
+			return DOMstrings;
 		}
 	}
 
 })()
+
+
+
+
 
 // publicTest() should return sth so that it can be saved 
 // in a variable, or it wont work 
@@ -29,6 +45,7 @@ var UIController = (function(){
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+	var DOM = UICtrl.getDOMstrings();
 
 	var ctrlAddItem = function(){
 		// 1. get input data
@@ -48,7 +65,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
 // click button or press enter same result
 // older browsers dont have keyCode property, use which
-	document.querySelector('.add__btn').addEventListener('click',ctrlAddItem)
+	document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
 
 	document.addEventListener('keypress',function(event){
 		if(event.keyCode === 13 || event.which === 13){
